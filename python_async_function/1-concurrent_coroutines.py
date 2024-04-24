3a9956651a5195aa5957965face6b6b1cf0cf466
root <root@LAPTOP-2LV4F0PM>
2024-04-24 11:15:31 +0200

    wait random

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" execute multiple coroutines at the same time """
 import asyncio
 from typing import List
-
-
-""" execute multiple coroutines at the same time """
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ returns the list of all the delays (float values). """
     delays = []
 
     coroutines = [wait_random(max_delay) for _ in range(n)]
