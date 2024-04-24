d9a617fcd0d5182f4bccb6b9d55d390205b04d83
root <root@LAPTOP-2LV4F0PM>
2024-04-24 09:36:44 +0200

    concatenate twos trings

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Concatenate two strings and return the result."""
 
 
 def concat(str1: str, str2: str) -> str:
+    """ Concatenate two strings """
     return str1 + str2
