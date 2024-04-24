a279d753c40f6004e86896842cf08f2409b9572e
root <root@LAPTOP-2LV4F0PM>
2024-04-24 09:46:41 +0200

    string representation of a float

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 
 """Return the string representation of a float."""
 
 
 def to_str(n: float) -> str:
+    """Return str of a float."""
     return str(n)
