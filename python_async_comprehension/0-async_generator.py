bd67dffe57a86640eedd4bc6ed219f7456879004
root <root@LAPTOP-2LV4F0PM>
2024-04-25 20:22:02 +0200

    async generator yield number

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,12 @@
 """ Import modules """
 import asyncio
 import random
-from typing import List
 
-async def async_generator() -> List[float]:
+
+async def async_generator() -> 'AsyncIterator[float]':
     """ Asynchrone function that loop 10 times and
     yield number between 0 and 10 """
     random_numbers = []
-    async for _ in range(1, 11):
+    for _ in range(10):
         await asyncio.sleep(1)
-        random_number = random.randint(0, 10)
-        random_numbers.append(random_number)
-    return random_numbers
+        yield random.uniform(0, 10)
