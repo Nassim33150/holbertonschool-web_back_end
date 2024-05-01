3bea20e583c20ae4cd9b73470aca6ad3d867d5de
root <root@LAPTOP-2LV4F0PM>
2024-05-01 11:33:31 +0200

    add condition if > 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,9 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     """ function should return a tuple
     of size two containing a start
     index and an end index """
-    debut_index = (page - 1) * page_size
-    end_index = page * page_size
+    debut_index, end_index = 0, 0
+
+    if page > 0 and page_size > 0:
+        debut_index = (page - 1) * page_size
+        end_index = page * page_size
     return debut_index, end_index
