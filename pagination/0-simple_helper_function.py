bda676c2943ae19d8334237c8fda4bade3c218ce
root <root@LAPTOP-2LV4F0PM>
2024-05-01 11:26:13 +0200

    index of pages

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """ function should return a tuple
+    of size two containing a start
+    index and an end index """
+    debut_index = (page - 1) * page_size
+    end_index = page * page_size
+    return debut_index, end_index
