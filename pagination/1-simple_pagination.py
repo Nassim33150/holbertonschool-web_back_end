0957f1bf854d6e3ab8121c7bc048004f311cc2f2
root <root@LAPTOP-2LV4F0PM>
2024-05-02 10:04:28 +0200

    update function assert

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,11 +1,19 @@
 """ Import modules """
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
-""" import index_range """
-index_range = __import__('0-simple_helper_function').index_range
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ function should return a tuple
+    of size two containing a start
+    index and an end index """
+    debut_index, end_index = 0, 0
+
+    if page > 0 and page_size > 0:
+        debut_index = (page - 1) * page_size
+        end_index = page * page_size
+    return debut_index, end_index
 
 
 class Server:
@@ -32,10 +40,10 @@ class Server:
         to paginate the dataset correctly
         and return the appropriate
         page of the dataset """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        dataset = self.dataset()
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
         start_index, end_index = index_range(page=page, page_size=page_size)
+        dataset = self.dataset()
         if start_index >= len(dataset):
             return []
         return dataset[start_index:end_index]
