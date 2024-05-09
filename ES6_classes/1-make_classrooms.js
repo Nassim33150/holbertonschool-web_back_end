be0da3b949d6e411521e2d46c9202777d8ec9145
root <root@LAPTOP-2LV4F0PM>
2024-05-09 13:33:17 +0200

    create objects rooms

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+// Import ClassRoom
+import ClassRoom from "./0-classroom.js"
+
+// Implement a function named initializeRooms. It should return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order).
+function initializeRooms() {
+	const room1 = newClassRoom(19);
+	const room2 = newClassRoom(20);
+	const room3 = newClassRoom(34);
+	
+	return [object1._maxStudentSize, object2._maxStudentSize, object3._maxStudentSize];
+}
