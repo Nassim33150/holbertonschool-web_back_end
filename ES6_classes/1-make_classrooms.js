22bde4002fefcaa157679b677338585e3739307c
root <abaida.nassim33@email.com>
2024-05-09 14:38:38 +0000

    update styntax

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -3,9 +3,11 @@ import ClassRoom from "./0-classroom.js"
 
 // Implement a function named initializeRooms. It should return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order).
 function initializeRooms() {
-	const room1 = newClassRoom(19);
-	const room2 = newClassRoom(20);
-	const room3 = newClassRoom(34);
-	
-	return [object1._maxStudentSize, object2._maxStudentSize, object3._maxStudentSize];
+	const room1 = new ClassRoom(19);
+	const room2 = new ClassRoom(20);
+	const room3 = new ClassRoom(34);
+
+	return [room1, room2, room3];
 }
+
+export default initializeRooms;
