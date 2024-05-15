a321fb793ae0d9eca4631242a8c433cb512d561b
root <root@LAPTOP-2LV4F0PM>
2024-05-15 20:58:45 +0200

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,9 +1,6 @@
 export function handleResponseFromAPI(promise) {
-	promise.then(() => {
-		console.log('Got a response from the API');
-		return { status: 200, body: 'success' };
-	}).catch(() => {
-		console.log('Got an error response from the API');
-		return new Error();
-	});
+	return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
