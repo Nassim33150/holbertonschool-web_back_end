10be69baa7ee3400fd951c3bd338eb3437258581
root <root@LAPTOP-2LV4F0PM>
2024-05-21 10:18:17 +0200

    task0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export function getListStudents() {
+  const students = [
+    { id: 1, firstname: "Guillaume", location: "San Francisco" },
+    { id: 2, firstname: "James", location: "Columbia" },
+    { id: 5, firstname: "Serena", location: "San Francisco" }
+  ];
+
+  console.log(students);
+}
