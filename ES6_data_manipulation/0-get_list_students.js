35ab6698237c6fa6b203e944d466d3d617ca4def
root <root@LAPTOP-2LV4F0PM>
2024-05-24 11:08:24 +0200

    edit const

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,21 @@
-export function getListStudents() {
-  const students = [
-    { id: 1, firstname: "Guillaume", location: "San Francisco" },
-    { id: 2, firstname: "James", location: "Columbia" },
-    { id: 5, firstname: "Serena", location: "San Francisco" }
-  ];
+export default function getListStudents() {
+  const o1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
 
-  console.log(students);
+  const o2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const o3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [o1, o2, o3];
 }
